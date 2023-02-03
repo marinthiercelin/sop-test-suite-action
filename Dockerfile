c7f6b3738c77d50a722d7a74bf810fab35cd538d
M. Thiercelin <marin.thiercelin@proton.ch>
2023-02-03 15:53:11 +0100

    Use ubuntu as base

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rust
+FROM ubuntu
 
 # Build test suite
 
--- run_test_suite.sh
+++ run_test_suite.sh
@@ -4,6 +4,4 @@ set -x
 CONFIG_PATH=$1
 RESULTS_PATH=$2
 
-cp -r ${TEST_SUITE_DIR}
-
 $TEST_SUITE --config $CONFIG_PATH --json-out $RESULTS_PATH
